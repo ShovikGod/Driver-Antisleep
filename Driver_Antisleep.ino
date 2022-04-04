cba2e2416911765ef51346fbac97d1714555686b
ShovikGod <shovikmetal@gmail.com>
2022-04-04 21:04:55 +0530

    updated after circuit complete

--- Driver_Antisleep.ino
+++ Driver_Antisleep.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 
-const int MPU_addr=0x68;
+const int MPU_addr = 0x68;
 int16_t axis_X, axis_Y, axis_Z;
-int minVal=265, maxVal=402;
+int minVal = 265, maxVal = 402;
 double x, y, z;
-int LED=2;
+unsigned long start=0, previous=0;
+unsigned int BUZZER = 2, LED_R = 5, LED_G = 6, LED_R_state=1;
 
-void setup(){
-  pinMode(LED,OUTPUT);
+void setup() {
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);
@@ -15,26 +18,37 @@ void setup(){
   Wire.endTransmission(true);
   Serial.begin(115200);
 }
-void loop(){
+void loop() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);
-  axis_X=Wire.read()<<8|Wire.read();
-  axis_Y=Wire.read()<<8|Wire.read();
-  axis_Z=Wire.read()<<8|Wire.read();
-  int xAng=map(axis_X,minVal,maxVal,-90,90);
-  int yAng=map(axis_Y,minVal,maxVal,-90,90);
-  int zAng=map(axis_Z,minVal,maxVal,-90,90);
-  z=RAD_TO_DEG*(atan2(-yAng,-xAng)+PI);
-  Serial.println(z);
-  if(z>=255 && z<=290){
+  Wire.requestFrom(MPU_addr, 14, true);
+  axis_X = Wire.read() << 8 | Wire.read();
+  axis_Y = Wire.read() << 8 | Wire.read();
+  axis_Z = Wire.read() << 8 | Wire.read();
+  int yAng = map(axis_Y, minVal, maxVal, -90, 90);
+  int zAng = map(axis_Z, minVal, maxVal, -90, 90);
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  Serial.println(x);
+  if (x >= 235 && x <= 300) {
     Serial.println("OFF!!");
-    digitalWrite(LED,LOW);
+    digitalWrite(BUZZER, LOW);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_R, HIGH);
   }
-  else{
+  else {
     Serial.println("ON!!");
-    digitalWrite(LED,HIGH);
+    digitalWrite(BUZZER, HIGH);
+    digitalWrite(LED_G, HIGH);
+    start = millis();
+    if (start-previous>100){
+      previous=start;
+      if (LED_R_state==0)
+        LED_R_state=1;
+      else
+        LED_R_state=0;
+      digitalWrite(LED_R, LED_R_state);
+    }
   }
-  delay(100);
+  delay(50);
 }
